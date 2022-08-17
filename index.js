f682874408909859ad3c35077467369ceca0d2e5
Troy V Washington <103076891+TVW96@users.noreply.github.com>
2022-08-17 16:39:34 +0000

    users page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var data = require('./data/test.json');
 
 app.set('view engine','ejs')
 
@@ -10,35 +11,38 @@ app.use(express.static('images'));
 app.get('/', (req, res) => {
   var title = "My Home Page";
   var heading = "My Website";
-  var image = 'img src="images/sipurple.png" width="200" height="200"/';
   //res.send('Ola from Express!')
   res.render('pages/index',{
     'title':title,
-    'heading':heading,
-    'image':image
-  
+    'heading':heading
   })
 })
 
 app.get('/about', (req, res) => {
   var title = "My About Page";
   var heading = "My Website";
-  var image = 'img src="images/siBlack.png" width="200" height="200"/';
   res.render('pages/about',{
     'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/users', (req, res) => {
+  var title = "My Users Page";
+  var heading = "My Website";
+  res.render('users/index',{
+    'title':title,
     'heading':heading,
-    'image':image
+    'users':data
   })
 })
 
 app.get('/apps', (req, res) => {
   var title = "My Apps";
   var heading = "My Website";
-  var image = 'img src="images/sired.png" width="200" height="200"/';
   res.render('pages/apps',{
     'title':title,
-    'heading':heading,
-    'image':image
+    'heading':heading
   })
 })
 
@@ -47,14 +51,13 @@ app.get('/apps', (req, res) => {
 app.get('/contact', (req, res) => {
   var title = "My Contact Page";
   var heading = "My Website";
-  var image = 'img src="images/copmuhead.jpeg" width="200" height="200"/';
   res.render('pages/contact',{
     'title':title,
-    'heading':heading,
-    'image':image
+    'heading':heading
   })
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log(data);
 })
